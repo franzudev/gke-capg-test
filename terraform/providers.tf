cc3f2dd4fc091cb1ce37dbf7423964c7a2674575
Federico François <me@franzu.dev>
2025-05-03 23:12:47 +0200

    fix: declare providers

---
+++ terraform/providers.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.10.1"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.33.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.36.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.17.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.gke.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "gke-gcloud-auth-plugin"
+    }
+  }
+}
